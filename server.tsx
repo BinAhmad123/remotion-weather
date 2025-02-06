e458ed9df9df09a39751e87587dd3c4258dddc0d
Talented-Dev934 <20168172+Talented-Dev934@users.noreply.github.com>
2025-02-06 04:18:48 -0500

    fix_issue#5

--- server.tsx
+++ server.tsx
@@ -1,10 +1,3 @@
-/**
- * This is an example of a server that returns dynamic video.
- * Run `npm run server` to try it out!
- * If you don't want to render videos on a server, you can safely
- * delete this file.
- */
-
 import { bundle } from '@remotion/bundler';
 import { getCompositions, renderFrames, stitchFramesToVideo } from '@remotion/renderer';
 import express from 'express';
